c068e598841cd85c2839c8c6e57f6dc2a9ad999a
Grzegorz Marek Wardaszka <01159496@pw.edu.pl>
2021-06-22 09:58:17 +0200

    1 commit

--- src/GOL/Cell.java
+++ src/GOL/Cell.java
@@ -1,6 +1,5 @@
 package GOL;
 
-import java.util.ArrayList;
 import java.util.Random;
 
 public class Cell {
@@ -21,11 +20,40 @@ public class Cell {
     private int col;
     private String colour;
     private String[] colours = {"white", "black"};
+    private State previousState;
+    private State nextState;
+
+
 
     public Cell(int row, int col) {
         this.col = col;
         this.row = row;
         Random random = new Random();
         this.colour = String.valueOf(random.nextInt(colours.length));
+        if (this.colour.equals("1")){
+            this.nextState = State.ALIVE;
+            //this.previousState = State.ALIVE;//
+
+        }
+        else{
+            this.nextState = State.DEAD;
+            //this.previousState = State.DEAD; //
+        }
+    }
+
+    public State getPreviousState() {
+        return previousState;
+    }
+
+    public void setPreviousState(State previousState) {
+        this.previousState = previousState;
+    }
+
+    public State getNextState() {
+        return nextState;
+    }
+
+    public void setNextState(State nextState) {
+        this.nextState = nextState;
     }
 }
--- src/GOL/start.java
+++ src/GOL/start.java
@@ -2,15 +2,17 @@ package GOL;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.concurrent.TimeUnit;
 
 public class start {
     private Integer size;
     private JFrame frame;
     private Cell[][] cellList;
+
     public start() {
         size = 50;
         frame = new JFrame();
-        frame.setSize(965,990);
+        frame.setSize(965, 990);
         cellList = new Cell[50][50];
         createCanvas(size, cellList);
         grid board = new grid();
@@ -23,6 +25,7 @@ public class start {
 
 
     }
+
     class grid extends JPanel {
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
@@ -32,50 +35,74 @@ public class start {
             for (int b = 0; b < size; b++) {
                 for (int i = 0; i < size; i++) {
 
-                    //if (cellList[i][b].getWalls()[0]) {
-                    g.drawPolyline(new int[]{i * value, i * value + value}, new int[]{b * value, b * value}, 2);
-                    //}
-                    //if (cellList[i][b].getWalls()[1]) {
-                    g.drawPolyline(new int[]{i * value + value, i * value + value}, new int[]{b * value, b * value + value}, 2);
-                    //}
-                    //if (cellList[i][b].getWalls()[2]) {
-                    g.drawPolyline(new int[]{i * value + value, i * value}, new int[]{b * value + value, b * value + value}, 2);
-                    //}
-                    //if (cellList[i][b].getWalls()[3]) {
-                    g.drawPolyline(new int[]{i * value, i * value}, new int[]{b * value + value, b * value}, 2);
-                    // }
-                    g.setColor(Color.black);
-                    System.out.println(cellList[i][b].getColour());
-                    if (cellList[i][b].getColour().equals("1")){
+                    //System.out.println(cellList[i][b].getColour());
+                    if (cellList[i][b].getNextState()== State.DEAD) {
+
+                        g.setColor(Color.black);
                         //g.fillRect(value);
                         g.fillRect((int) (value * cellList[i][b].getRow()), (int) (value * cellList[i][b].getCol()), (int) (value), (int) (value));
                     }
-                }
 
-                }
-
-
-        }
-    }
-
-        public void createCanvas(Integer size, Cell[][] cells) {
-            for (int b = 0; b < size; b++) {
-                for (int i = 0; i < size; i++) {
-                    cells[i][b] = new Cell(i, b);
-                }
-            }
-
-        }
-
-        public static void main(String[] args) {
-
-            SwingUtilities.invokeLater(
-                    () -> {
-                        start g = new start();
+                    if (cellList[i][b].getNextState()== State.ALIVE) {
+                        g.setColor(Color.white);
+                        g.fillRect(30,30,50,50);
+                        //g.fillRect(value);
+                        g.fillRect((int) (value * cellList[i][b].getRow()), (int) (value * cellList[i][b].getCol()), (int) (value), (int) (value));
                     }
-            );
 
+                    cellList[i][b].setPreviousState(cellList[i][b].getNextState());
+                    //System.out.println(cellList[i][b].getNextState());
+                }
+
+            }
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("HEJ");
+            nextState();
         }
-
-
     }
+
+    public void nextState(){
+        for (int y = 0; y < size; y++) {
+            for (int x = 0; x < size ; x++) {
+                int aliveNeighbourhoods = 0;
+                    for (int i = y-1; i < y+1; i++) {
+                        for (int j = x-1; j < x+1; j++) {
+                            if (i ==y && j ==x) continue;
+                            try {
+                                if (cellList[i][j].getPreviousState() == State.ALIVE) aliveNeighbourhoods++;
+                            } catch (Exception ignored){
+                            }
+
+                        }
+                    }
+                    if (cellList[y][x].getPreviousState()==State.DEAD && aliveNeighbourhoods == 3) cellList[y][x].setNextState(State.ALIVE);
+                    else if (cellList[y][x].getPreviousState()==State.ALIVE && (aliveNeighbourhoods < 2 || aliveNeighbourhoods > 3)) cellList[y][x].setNextState(State.DEAD);
+                    else cellList[y][x].setNextState(cellList[y][x].getPreviousState());
+            }
+        }
+        SwingUtilities.updateComponentTreeUI(frame);
+    }
+
+    public void createCanvas(Integer size, Cell[][] cells) {
+        for (int b = 0; b < size; b++) {
+            for (int i = 0; i < size; i++) {
+                cells[i][b] = new Cell(i, b);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(
+                () -> {
+                    start g = new start();
+
+
+                }
+        );
+    }
+}
