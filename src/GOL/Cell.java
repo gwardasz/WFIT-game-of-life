588ef8d4fef45971dfb77bac34fde8964b8e83b3
Grzegorz Marek Wardaszka <01159496@pw.edu.pl>
2021-06-22 12:30:08 +0200

    2 commit

--- src/GOL/Cell.java
+++ src/GOL/Cell.java
@@ -1,44 +1,93 @@
 package GOL;
 
+import java.util.ArrayList;
 import java.util.Random;
 
+enum State{
+    ALIVE, DEAD
+}
 public class Cell {
     private int row;
-
-    public String getColour() {
-        return colour;
-    }
-
-    public int getRow() {
-        return row;
-    }
-
-    public int getCol() {
-        return col;
-    }
-
     private int col;
-    private String colour;
-    private String[] colours = {"white", "black"};
     private State previousState;
     private State nextState;
-
+    private final boolean alive = new Random().nextBoolean();
 
 
     public Cell(int row, int col) {
         this.col = col;
         this.row = row;
-        Random random = new Random();
-        this.colour = String.valueOf(random.nextInt(colours.length));
-        if (this.colour.equals("1")){
-            this.nextState = State.ALIVE;
-            //this.previousState = State.ALIVE;//
+        /*
+        if ((this.row == 39 || this.row == 40 || this.row == 41)
+                && (this.col == 39 || this.col == 40 || this.col == 41))
+        this.nextState = State.ALIVE;
+         */
+        if (alive) this.nextState = State.ALIVE;
+        else this.nextState = State.DEAD;
+
+
+
+    }
+    public Cell checkNegative(int row, int col, Cell[][] celllist) {
+        if (col < 0 || row < 0 || col > celllist.length - 1 || row > celllist.length - 1) {
+            return null;
+        } else {
+            return celllist[row][col];
 
         }
-        else{
-            this.nextState = State.DEAD;
-            //this.previousState = State.DEAD; //
+    }
+    public Integer checkNeighbors(Cell[][] celllist) {
+        ArrayList<Cell> neighbors = new ArrayList<>();
+        Cell top = checkNegative(row, col - 1, celllist);
+
+        Cell right = checkNegative(row + 1, col, celllist);
+
+        Cell bottom = checkNegative(row, col + 1, celllist);
+
+        Cell left = checkNegative(row - 1, col, celllist);
+
+        Cell topright = checkNegative(row + 1, col - 1, celllist);
+
+        Cell topleft = checkNegative(row - 1, col - 1, celllist);
+
+        Cell bottomright = checkNegative(row + 1, col + 1, celllist);
+
+        Cell bottomleft = checkNegative(row - 1, col + 1, celllist);
+
+
+        if (top != null && top.getPreviousState()== State.ALIVE) {
+            neighbors.add(top);
+
         }
+        if (right != null && right.getPreviousState()== State.ALIVE) {
+            neighbors.add(right);
+
+        }
+        if (bottom != null && bottom.getPreviousState()== State.ALIVE) {
+            neighbors.add(bottom);
+
+        }
+        if (left != null && left.getPreviousState()== State.ALIVE) {
+            neighbors.add(left);
+
+        }
+        if (topright != null && topright.getPreviousState()== State.ALIVE) {
+            neighbors.add(topright);
+
+        }
+        if (topleft != null && topleft.getPreviousState()== State.ALIVE) {
+            neighbors.add(topleft);
+
+        }
+        if (bottomright != null && bottomright.getPreviousState()== State.ALIVE) {
+            neighbors.add(bottomright);
+
+        }
+        if (bottomleft != null && bottomleft.getPreviousState()== State.ALIVE) {
+            neighbors.add(bottomleft);
+
+        }
+        return neighbors.size();
     }
 
     public State getPreviousState() {
@@ -55,5 +104,12 @@ public class Cell {
 
     public void setNextState(State nextState) {
         this.nextState = nextState;
+    }
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
     }
 }
--- src/GOL/start.java
+++ src/GOL/start.java
@@ -5,20 +5,17 @@ import java.awt.*;
 import java.util.concurrent.TimeUnit;
 
 public class start {
-    private Integer size;
     private JFrame frame;
     private Cell[][] cellList;
+    public final int size = 80;
 
     public start() {
-        size = 50;
         frame = new JFrame();
         frame.setSize(965, 990);
-        cellList = new Cell[50][50];
-        createCanvas(size, cellList);
-        grid board = new grid();
-        frame.add(board, BorderLayout.CENTER);
+        cellList = new Cell[size][size];
+        createCanvas(cellList);
+        frame.add(new grid(), BorderLayout.CENTER);
         frame.setLocationRelativeTo(null);
-
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.setTitle("Game of life");
         frame.setVisible(true);
@@ -27,40 +24,32 @@ public class start {
     }
 
     class grid extends JPanel {
-        public void paintComponent(Graphics g) {
+        public void paintComponent(Graphics g){
             super.paintComponent(g);
-            Integer value = (Integer) 990 / size;
+            int value = 990 / size;
 
 
-            for (int b = 0; b < size; b++) {
-                for (int i = 0; i < size; i++) {
-
-                    //System.out.println(cellList[i][b].getColour());
-                    if (cellList[i][b].getNextState()== State.DEAD) {
-
+            for (int y = 0; y < size; y++) {
+                for (int x = 0; x < size; x++) {
+                    // mam pytanie w tej pętli czy get getRow i getCol to nie powinny gbyć odwrotnie?
+                    // bo w sumie kolumna wskazuje na x a rząd na y
+                    if (cellList[y][x].getNextState()== State.DEAD) {
                         g.setColor(Color.black);
-                        //g.fillRect(value);
-                        g.fillRect((int) (value * cellList[i][b].getRow()), (int) (value * cellList[i][b].getCol()), (int) (value), (int) (value));
+                        g.fillRect(value * cellList[y][x].getRow(), value * cellList[y][x].getCol(), value, value);
                     }
 
-                    if (cellList[i][b].getNextState()== State.ALIVE) {
+                    if (cellList[y][x].getNextState()== State.ALIVE) {
                         g.setColor(Color.white);
-                        g.fillRect(30,30,50,50);
-                        //g.fillRect(value);
-                        g.fillRect((int) (value * cellList[i][b].getRow()), (int) (value * cellList[i][b].getCol()), (int) (value), (int) (value));
+                        g.fillRect(value * cellList[y][x].getRow(), value * cellList[y][x].getCol(), value, value);
                     }
-
-                    cellList[i][b].setPreviousState(cellList[i][b].getNextState());
-                    //System.out.println(cellList[i][b].getNextState());
+                    cellList[y][x].setPreviousState(cellList[y][x].getNextState());
                 }
-
             }
+            /*
             try {
-                TimeUnit.SECONDS.sleep(2);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println("HEJ");
+                TimeUnit.SECONDS.sleep((long) 1.5);
+            } catch (InterruptedException ignored){}
+             */
             nextState();
         }
     }
@@ -68,41 +57,34 @@ public class start {
     public void nextState(){
         for (int y = 0; y < size; y++) {
             for (int x = 0; x < size ; x++) {
-                int aliveNeighbourhoods = 0;
-                    for (int i = y-1; i < y+1; i++) {
-                        for (int j = x-1; j < x+1; j++) {
-                            if (i ==y && j ==x) continue;
-                            try {
-                                if (cellList[i][j].getPreviousState() == State.ALIVE) aliveNeighbourhoods++;
-                            } catch (Exception ignored){
-                            }
-
-                        }
+                int aliveNeighbourhoods = cellList[y][x].checkNeighbors(cellList);
+                    if (cellList[y][x].getPreviousState()==State.DEAD &&
+                            aliveNeighbourhoods == 3){
+                        cellList[y][x].setNextState(State.ALIVE);
                     }
-                    if (cellList[y][x].getPreviousState()==State.DEAD && aliveNeighbourhoods == 3) cellList[y][x].setNextState(State.ALIVE);
-                    else if (cellList[y][x].getPreviousState()==State.ALIVE && (aliveNeighbourhoods < 2 || aliveNeighbourhoods > 3)) cellList[y][x].setNextState(State.DEAD);
+
+                    else if (cellList[y][x].getPreviousState()==State.ALIVE &&
+                            aliveNeighbourhoods < 2 ||
+                            aliveNeighbourhoods > 3)
+                        cellList[y][x].setNextState(State.DEAD);
                     else cellList[y][x].setNextState(cellList[y][x].getPreviousState());
             }
         }
         SwingUtilities.updateComponentTreeUI(frame);
     }
 
-    public void createCanvas(Integer size, Cell[][] cells) {
-        for (int b = 0; b < size; b++) {
-            for (int i = 0; i < size; i++) {
-                cells[i][b] = new Cell(i, b);
+    public void createCanvas(Cell[][] cells) {
+        for (int y = 0; y < size; y++) {
+            for (int x = 0; x < size; x++) {
+                cells[y][x] = new Cell(y, x);
             }
         }
     }
 
+
     public static void main(String[] args) {
-
         SwingUtilities.invokeLater(
-                () -> {
-                    start g = new start();
-
-
-                }
+                start::new
         );
     }
 }
