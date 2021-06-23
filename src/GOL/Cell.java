0591e4b1c693a65fefe12808120091736fddf5cd
Grzegorz Marek Wardaszka <01159496@pw.edu.pl>
2021-06-23 09:57:53 +0200

    4 commit

--- src/GOL/Cell.java
+++ src/GOL/Cell.java
@@ -5,48 +5,46 @@ import java.util.Random;
 enum State{
     ALIVE, DEAD
 }
+
 public class Cell {
     private final int row;
     private final int col;
     private State previousState;
     private State nextState;
 
-
     public Cell(int row, int col) {
         this.col = col;
         this.row = row;
-        /*
-        if ((this.row == 39 || this.row == 40 || this.row == 41)
-                && (this.col == 39 || this.col == 40 || this.col == 41))
+
+        /* test init
+        if ((this.row%7 == 0 || this.row == 50 || this.row == 51)
+                && (this.col%4 == 0 || this.col == 50 || this.col == 51))
         this.nextState = State.ALIVE;
          */
+
         boolean alive = new Random().nextBoolean();
         if (alive) this.nextState = State.ALIVE;
         else this.nextState = State.DEAD;
-
-
-
     }
-    public Cell checkNegative(int row, int col, Cell[][] cellArr) {
-        if (col < 0 || row < 0 || col > cellArr.length - 1 || row > cellArr.length - 1) {
-            return null;
-        } else {
-            return cellArr[row][col];
 
-        }
+    public Cell checkNegative(int row, int col) {
+        if (col < 0 || row < 0 || col > start.getCellArr().length - 1 ||
+                row > start.getCellArr().length - 1) return null;
+        else return start.getCellArr()[row][col];
     }
-    public Integer checkNeighbors(Cell[][] cellArr) {
+
+    public Integer checkNeighbors() {
         int aliveNeighbors = 0;
         Cell [] neighbours = new Cell[8];
 
-        neighbours[0] = checkNegative(row, col - 1, cellArr); //top
-        neighbours[1] = checkNegative(row + 1, col, cellArr); //right
-        neighbours[2] = checkNegative(row, col + 1, cellArr); //bottom
-        neighbours[3] = checkNegative(row - 1, col, cellArr); // left
-        neighbours[4] = checkNegative(row + 1, col - 1, cellArr); // topRight
-        neighbours[5] = checkNegative(row - 1, col - 1, cellArr); // topLeft
-        neighbours[6] = checkNegative(row + 1, col + 1, cellArr); // bottomRight
-        neighbours[7] = checkNegative(row - 1, col + 1, cellArr); // bottomLeft
+        neighbours[0] = checkNegative(row, col - 1); //top
+        neighbours[1] = checkNegative(row + 1, col); //right
+        neighbours[2] = checkNegative(row, col + 1); //bottom
+        neighbours[3] = checkNegative(row - 1, col); // left
+        neighbours[4] = checkNegative(row + 1, col - 1); // topRight
+        neighbours[5] = checkNegative(row - 1, col - 1); // topLeft
+        neighbours[6] = checkNegative(row + 1, col + 1); // bottomRight
+        neighbours[7] = checkNegative(row - 1, col + 1); // bottomLeft
 
         for (Cell neighbour: neighbours){
             if (neighbour != null && neighbour.getPreviousState()==State.ALIVE) aliveNeighbors++;
@@ -77,9 +75,4 @@ public class Cell {
     public int getRow() {
         return row;
     }
-
-        }
-
-
-
-
+}
--- src/GOL/start.java
+++ src/GOL/start.java
@@ -2,25 +2,26 @@ package GOL;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.concurrent.TimeUnit;
 
 public class start {
     private final JFrame frame;
-    private final Cell[][] cellArr;
+    private static Cell[][] cellArr;
     public final int size = 100;
+
+    public static Cell[][] getCellArr() {
+        return cellArr;
+    }
 
     public start() {
         frame = new JFrame();
         frame.setSize(915, 937);
         cellArr = new Cell[size][size];
-        createCanvas(cellArr);
+        createCanvas();
         frame.add(new grid(), BorderLayout.CENTER);
         frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Game of life");
         frame.setVisible(true);
-
-
     }
 
     class grid extends JPanel {
@@ -28,27 +29,22 @@ public class start {
             super.paintComponent(g);
             int value = 990 / size;
 
-
             for (int y = 0; y < size; y++) {
                 for (int x = 0; x < size; x++) {
 
-                    if (cellArr[y][x].getNextState()== State.DEAD) {
-                        g.setColor(Color.black);
-                        g.fillRect(value * cellArr[y][x].getCol(), value * cellArr[y][x].getRow(), value, value);
-                    }
-
                     if (cellArr[y][x].getNextState()== State.ALIVE) {
                         g.setColor(Color.white);
-                        g.fillRect(value * cellArr[y][x].getCol(), value * cellArr[y][x].getRow(), value, value);
+                    } else {
+                        g.setColor(Color.black);
                     }
+                    g.fillRect(value * cellArr[y][x].getCol(), value * cellArr[y][x].getRow(), value, value);
                     cellArr[y][x].setPreviousState(cellArr[y][x].getNextState());
                 }
             }
+
             try {
                 Thread.sleep(15);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            } catch (InterruptedException ignored) {}
 
             nextState();
         }
@@ -57,30 +53,26 @@ public class start {
     public void nextState(){
         for (int y = 0; y < size; y++) {
             for (int x = 0; x < size ; x++) {
-                int aliveNeighbours = cellArr[y][x].checkNeighbors(cellArr);
+                int aliveNeighbours = cellArr[y][x].checkNeighbors();
                     if (cellArr[y][x].getPreviousState()==State.DEAD &&
-                            aliveNeighbours == 3){
-                        cellArr[y][x].setNextState(State.ALIVE);
-                    }
+                            aliveNeighbours == 3) cellArr[y][x].setNextState(State.ALIVE);
 
                     else if (cellArr[y][x].getPreviousState()==State.ALIVE &&
-                            aliveNeighbours < 2 ||
-                            aliveNeighbours > 3)
-                        cellArr[y][x].setNextState(State.DEAD);
+                            aliveNeighbours < 2 || aliveNeighbours > 3) cellArr[y][x].setNextState(State.DEAD);
+
                     else cellArr[y][x].setNextState(cellArr[y][x].getPreviousState());
             }
         }
         SwingUtilities.updateComponentTreeUI(frame);
     }
 
-    public void createCanvas(Cell[][] cells) {
+    public void createCanvas() {
         for (int y = 0; y < size; y++) {
             for (int x = 0; x < size; x++) {
-                cells[y][x] = new Cell(y, x);
+                cellArr[y][x] = new Cell(y, x);
             }
         }
     }
-
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(
